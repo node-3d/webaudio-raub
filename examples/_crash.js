4805cbacf9e51a55f1cb312fc00332c7efc30a5b
raub <raubtierxxx@gmail.com>
2018-05-16 14:01:49 +0300

    Debug panner crash

--- examples/_crash.js
+++ examples/_crash.js
@@ -9,8 +9,11 @@ const { AudioContext } = require('..');
 const read = require('./utils/read');
 
 
+const GC_LOCK = { _i: 0 };
+
 (async () => { try {
 	
+	const count = 6;
 	const objects = [];
 	const scene = [];
 	
@@ -19,85 +22,87 @@ const read = require('./utils/read');
 	const clip = await read(`${__dirname}/samples/hit.wav`);
 	
 	const gain = context.createGain();
-	const gain.connect( context.destination );
+	gain.connect( context.destination );
 	
 	const buffer = await new Promise(res => context.decodeAudioData(clip, b => res(b)));
 	
+	// GC_LOCK.objects = objects;
+	// GC_LOCK.scene = scene;
+	// GC_LOCK.context = context;
+	// GC_LOCK.clip = clip;
+	// GC_LOCK.gain = gain;
+	// GC_LOCK.buffer = buffer;
+	
 	// Create objects when audio buffer is loaded
 	for (let i = 0; i < count; i++) {
-		const s = i / count * Math.PI * 2;
-		const ball = new three.Mesh( ballGeometry, ballMaterial );
-		ball.castShadow = true;
-		ball.userData.down = false;
-		ball.position.x = radius * Math.cos( s );
-		ball.position.z = radius * Math.sin( s );
-		const audio = new three.PositionalAudio( listener );
-		audio.setBuffer( buffer );
-		audio._buf = buffer;
-		ball.add( audio );
-		scene.add( ball );
-		objects.push( ball );
+		const ball = { y: 0 };
+		const audio = {};
+		audio.panner = context.createPanner();
+		audio.panner.connect( gain );
+		audio.isPlaying = false;
+		audio.startTime = 0;
+		audio.playbackRate = 1;
+		audio.play = () => {
+			if ( audio.isPlaying === true ) {
+				
+				console.warn( 'Audio is already playing.' );
+				return;
+				
+			}
+			
+			var source = context.createBufferSource();
+			
+			source.buffer = buffer;
+			source.onended = () => audio.isPlaying = false;
+			source.playbackRate.setValueAtTime(audio.playbackRate, audio.startTime);
+			audio.startTime = context.currentTime;
+			source.start( audio.startTime, 0 );
+			
+			audio.isPlaying = true;
+			
+			audio.source = source;
+			
+			audio.source.connect( audio.panner );
+			
+		};
+		ball.audio = audio;
+		scene.push(ball);
+		objects.push(ball);
 	}
-	
-	const renderer = new three.WebGLRenderer( { antialias: true } );
-	renderer.shadowMap.enabled = true;
-	renderer.setSize( window.innerWidth, window.innerHeight );
-	renderer.setClearColor( 0x000000 );
-	renderer.setPixelRatio( window.devicePixelRatio );
-	
-	//
-	// const controls = new three.OrbitControls( camera, renderer.domElement );
-	// controls.minDistance = 1;
-	// controls.maxDistance = 25;
-	//
-	
-	window.addEventListener( 'resize', () => {
-		camera.aspect = window.innerWidth / window.innerHeight;
-		camera.updateProjectionMatrix();
-		renderer.setSize( window.innerWidth, window.innerHeight );
-	});
 	
 	const speed = 0.002;
 	const height = 3;
 	const offset = 0.5;
 	
 	const render = () => {
+		GC_LOCK._i++;
+		
 		const time = Date.now();
 		for ( let i = 0; i < objects.length; i++ ) {
 			const ball = objects[ i ];
-			const previousHeight = ball.position.y;
+			const previousHeight = ball.y;
 			const al = i * offset + ( time * speed );
 			
-			ball.position.y = Math.abs( Math.sin( al ) * height );
+			ball.y = Math.abs( Math.sin( al ) * height );
 			
-			if (ball.position.y === previousHeight) {
+			if (ball.y === previousHeight) {
 				continue;
 			}
 			
-			if ( ball.position.y < previousHeight ) {
-				ball.userData.down = true;
+			if ( ball.y < previousHeight ) {
+				ball.down = true;
 			} else {
 				// If ball changed direction from down to up
-				if ( ball.userData.down === true ) {
+				if ( ball.down === true ) {
 					
-					const audio = ball.children[ 0 ];
-					
-					// play audio with perfect timing when ball hits the surface
-					audio.play();
-					
-					ball.userData.down = false;
+					ball.audio.play();
+					ball.down = false;
 					
 				}
 			}
 		}
-		renderer.render( scene, camera );
 	};
 	
-	const animate = () => {
-		requestAnimationFrame( animate );
-		render();
-	};
-	
-	animate();
+	setInterval(() => render(), 16);
 	
 } catch (e) { console.error(e); } })();
